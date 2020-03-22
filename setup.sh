78be39877733d4799786833fa713931a567003f5
Yael Castro <cyaelcc94@gmail.com>
2020-03-22 16:51:49 -0600

    Fixing issue at restart in homeassistant container, and adding name in rhasspy container

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 echo "Hello!, installing HeyLupe!"
 echo "Installing Homeassistant"
 #Home Assistant Instalation
-docker run --init -d --name="home-assistant" -e "TZ=America/Mexico_City" -v $(pwd)/homeassistant/:/config --net=host homeassistant/raspberrypi3-homeassistant:stable
+docker run --init -d --restart unless-stopped --name="home-assistant" -e "TZ=America/Mexico_City" -v $(pwd)/homeassistant/:/config --net=host homeassistant/raspberrypi3-homeassistant:stable
 echo "Installing Rhasspy"
 #Rhasspy Instalation
-docker run -d -p 12101:12101 --restart unless-stopped -v $(pwd)/Rhasspy/profiles:/profiles --device /dev/snd:/dev/snd synesthesiam/rhasspy-server:latest --user-profiles /profiles --profile es
+docker run -d -p 12101:12101 --restart unless-stopped --name="rhasspy" -v $(pwd)/Rhasspy/profiles:/profiles --device /dev/snd:/dev/snd synesthesiam/rhasspy-server:latest --user-profiles /profiles --profile es
 
